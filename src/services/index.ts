98d28bbbe1e0176d51117c530757145306547cac
Gustavo Almeida <gu.almeidan2007@gmail.com>
2026-06-02 16:33:26 -0300

    adiciona exportação do serviço de transferências de exercícios, integrando-o ao módulo principal de serviços; melhora a organização e acessibilidade das funcionalidades relacionadas a transferências

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,5 +1,6 @@
 export * from "./periodizations.service";
 export * from "./exercises.service";
+export * from "./exerciseTransfers.service";
 export * from "./prs.service";
 export * from "./subscription.service";
 
